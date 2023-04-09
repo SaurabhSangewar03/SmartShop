58fc0b77a137412fd732a3170b97a6014b7bf00e
Saurabh Sangewar <sangewar007@gmail.com>
2023-04-09 17:09:45 +0530

    React setup and Chakra UI Setup

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import { extendTheme } from "@chakra-ui/react";
+import SmartShop from "./SmartShop";
+
+const theme = extendTheme({
+  colors: {
+    brand: {
+      100: "#f7fafc",
+    },
+  },
+  fonts: {
+    body: "Open Sans, sans-serif",
+    heading: "Montserrat, sans-serif",
+  },
+});
+
+function App() {
+  return (
+    <div>
+      <ChakraProvider theme={theme}>
+        <SmartShop />
+      </ChakraProvider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SmartShop.jsx
@@ -0,0 +1,13 @@
+import { Button, Heading } from "@chakra-ui/react";
+import React from "react";
+
+const SmartShop = () => {
+  return (
+    <>
+      <Heading>Hello SmartShop</Heading>
+      <Button colorScheme='orange'>Button</Button>
+    </>
+  );
+};
+
+export default SmartShop;
